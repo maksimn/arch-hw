217fde0e719226ca285c816a9039d8857712d665
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-04-08 07:16:18 +0300

    init

---
+++ ArchitectureHW/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ArchitectureHW
+//
+//  Created by Maksim Ivanov on 08/04/2019.
+//  Copyright © 2019 Maksim Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.yellow
+    }
+}
